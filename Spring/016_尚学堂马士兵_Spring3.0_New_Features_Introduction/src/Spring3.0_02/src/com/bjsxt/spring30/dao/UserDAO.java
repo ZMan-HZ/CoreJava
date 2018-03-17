47beff1ca4462f51adaeb967b1ba4fe71fe83bc1
Mann <manzhzh@163.com>
2018-03-17 11:07:44 +0800

    removeVideo

--- Spring/016_尚学堂马士兵_Spring3.0_New_Features_Introduction/src/Spring3.0_02/src/com/bjsxt/spring30/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.bjsxt.spring30.dao;
-
-import com.bjsxt.spring30.model.User;
-
-public interface UserDAO {
-	public void save(User u);
-}
--- Spring/016_尚学堂马士兵_Spring3.0_New_Features_Introduction/src/Spring3.0_04/src/com/bjsxt/spring30/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.bjsxt.spring30.service;
-
-import com.bjsxt.spring30.model.User;
-
-public interface UserService {
-	public void add(User u);
-}
